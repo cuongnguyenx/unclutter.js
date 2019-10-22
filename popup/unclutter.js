eafecaf0b287682a3a85c28ec06f6ce405484886
Arkinion <nicholasgr44@gmail.com>
2019-10-22 14:49:07 -0400

    Added buttons to the html dynamically

--- popup/unclutter.js
+++ popup/unclutter.js
@@ -1,1 +1,16 @@
+function getButtonIClick(buttonNum) {
+  console.log(toString(buttonNum));
+  return () => {
+    chrome.browserAction.setBadgeText({ text: buttonNum.toString() })
+  };
+}
 
+const content = document.getElementById("popup-content");
+
+for (let i = 0; i < 4; i++) {
+  let newButton = content.appendChild(document.createElement("div"));
+  newButton.textContent = "Button" + i;
+  newButton.classList.add("button");
+  newButton.classList.add("button" + i);
+  newButton.addEventListener("click", getButtonIClick(i));
+}
